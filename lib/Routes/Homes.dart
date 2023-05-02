8455527b22489b7f9091949d60783ca16a54d6fc
KJohnson82 <rojohnson@gmail.com>
2023-05-01 20:35:29 -0500

    Fixed the code overwrite I did earlier
    Co-authored-by: andreaborders <andreaborders@users.noreply.github.com>

--- lib/Routes/Homes.dart
+++ lib/Routes/Homes.dart
@@ -2,13 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import '../Theme/colorTheme.dart';
-import 'Rooms.dart';
-import '../../main.dart';
-// ignore: unused_import
+import '../main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'Rooms.dart';
-
-// var homeCount = 0;
+import '../firestore_instance.dart';
 
 // Home Class
 class Home {
@@ -29,22 +26,21 @@ class HomesPage extends StatefulWidget {
 
 class _HomesPageState extends State<HomesPage> {
   // Calls the list of already created home objects from the firestore database
-  final _homes = db.collection("homes");
+  final _homes = FirestoreInstance.getInstance().collection("homes");
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _homeNameController = TextEditingController();
   final FocusNode _focusNode = FocusNode();
 
-// Used to help keep count of the num of house objects
+  // Used to help keep count of the num of house objects
   int homeCount = 0;
 
-// Adds a new Home Object
+  // Adds a new Home Object
   void _addHome() async {
     if (_formKey.currentState!.validate()) {
       await _homes.add({
-        'homeName': _homeNameController.text,
+        'homeName': _homeNameController.text.toUpperCase(),
       });
       _homeNameController.clear();
-      Navigator.of(context).pop();
       // Sets the state after the object is created to cause the page to rebuild
       setState(() {});
       // Adds home to the homeCount list to keep track of number of objects
@@ -52,7 +48,7 @@ class _HomesPageState extends State<HomesPage> {
     }
   }
 
-// Allows you to change the name of an existing home object
+  // Allows you to change the name of an existing home object
   void _editHome(String documentId, String newName) async {
     if (_formKey.currentState!.validate()) {
       await _homes.doc(documentId).update({
@@ -63,9 +59,9 @@ class _HomesPageState extends State<HomesPage> {
     }
   }
 
-// Keeps track of the home objects count, the delay was to preventapp crashing
+  // Keeps track of the home objects count, the delay was to prevent app crashing
   void updateHomeCount(int count) async {
-    await Future.delayed(const Duration(milliseconds: 3));
+    await Future.delayed(const Duration(milliseconds: 1));
     setState(() {
       homeCount = count;
     });
@@ -75,6 +71,46 @@ class _HomesPageState extends State<HomesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // Streams the data from the firestore database. returns as snapshots
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        padding: const EdgeInsets.fromLTRB(5, 10, 10, 10),
+        color: homeventory.primary,
+        // surfaceTintColor: homeventory.primary,
+        elevation: 3,
+        notchMargin: 8,
+        shape: const AutomaticNotchedShape(
+          ContinuousRectangleBorder(),
+          StadiumBorder(),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(5, 0, 100, 0),
+              child: SizedBox(
+                height: 40,
+                width: 60,
+                child: Column(
+                  children: const [
+                    //Icons and Links will go here for other pages
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+              width: 60,
+              child: Column(
+                children: const [
+                  //Icons and Links will go here for other pages
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       body: StreamBuilder<QuerySnapshot>(
         stream: _homes.snapshots(),
         builder: (context, snapshot) {
@@ -83,7 +119,6 @@ class _HomesPageState extends State<HomesPage> {
           }
           final homes = snapshot.data!.docs;
           updateHomeCount(homes.length);
-
           return GridView.builder(
             scrollDirection: Axis.vertical,
             padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 50),
@@ -103,7 +138,7 @@ class _HomesPageState extends State<HomesPage> {
                             homeName: homes[index]['homeName'],
                           ),
                           type: PageTransitionType.rightToLeft,
-                          duration: Duration(milliseconds: 300)));
+                          duration: const Duration(milliseconds: 300)));
                 },
                 // Opens as dialog box to allow you to edit or delete a created object
                 onLongPress: () {
@@ -114,13 +149,14 @@ class _HomesPageState extends State<HomesPage> {
                           builder:
                               (BuildContext context, StateSetter setState) {
                             return AlertDialog(
+                              backgroundColor: homeventory.secondary,
                               title: Text(
                                   'Edit or Delete ${homes[index]["homeName"]}?'),
                               content: Form(
                                 key: _formKey,
                                 child: TextFormField(
                                   controller: _homeNameController,
-                                  decoration: InputDecoration(
+                                  decoration: const InputDecoration(
                                       labelText: 'Edit: Home Name'),
                                   validator: (value) {
                                     if (value == null || value.isEmpty) {
@@ -130,28 +166,47 @@ class _HomesPageState extends State<HomesPage> {
                                   },
                                 ),
                               ),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('Confirm'),
-                                  onPressed: () {
-                                    String documentId = homes[index].id;
-                                    _editHome(
-                                        documentId, _homeNameController.text);
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                TextButton(
-                                  child: const Text('Delete'),
-                                  onPressed: () {
-                                    String documentId = homes[index].id;
-                                    db
-                                        .collection('homes')
-                                        .doc(documentId)
-                                        .delete();
-                                    _homeNameController.clear();
-                                    Navigator.of(context).pop();
-                                    updateHomeCount(homeCount - 1);
-                                  },
+                              actions: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    TextButton(
+                                      child: Row(
+                                        children: const [
+                                          Text('Confirm  '),
+                                          Icon(Icons.check)
+                                        ],
+                                      ),
+                                      onPressed: () {
+                                        String documentId = homes[index].id;
+                                        _editHome(documentId,
+                                            _homeNameController.text);
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                    TextButton(
+                                      child: Row(
+                                        children: const [
+                                          Text('Delete  '),
+                                          Icon(Icons.delete)
+                                        ],
+                                      ),
+                                      onPressed: () {
+                                        String documentId = homes[index].id;
+                                        // db
+                                        FirestoreInstance.getInstance()
+                                            .collection('homes')
+                                            .doc(documentId)
+                                            .delete();
+                                        _homeNameController.clear();
+                                        Navigator.of(context).pop();
+                                        updateHomeCount(homeCount - 1);
+                                      },
+                                    ),
+                                  ],
                                 ),
                               ],
                             );
@@ -170,18 +225,10 @@ class _HomesPageState extends State<HomesPage> {
                     child: Container(
                       height: 50,
                       width: 50,
-                      // color: homeventory.secondary,
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
                         color: homeventory.secondary,
                         borderRadius: BorderRadius.circular(15),
-                        // boxShadow: const [
-                        //   BoxShadow(
-                        //       color: Colors.black12,
-                        //       spreadRadius: 1,
-                        //       blurRadius: 10,
-                        //       offset: Offset(2, 6))
-                        // ],
                         shape: BoxShape.rectangle,
                       ),
                       child: Text(
@@ -202,15 +249,14 @@ class _HomesPageState extends State<HomesPage> {
           );
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        // elevation: 3,
-        //Checks to see how many home objects exist and if it less than three allows you to add a new object, if not, it makes the button non-clickable
+      floatingActionButton: FloatingActionButton.large(
         onPressed: homeCount < 3
             ? () {
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
                     return AlertDialog(
+                      backgroundColor: homeventory.secondary,
                       title: const Text('Add A New Home'),
                       content: Form(
                         key: _formKey,
@@ -236,9 +282,9 @@ class _HomesPageState extends State<HomesPage> {
                         TextButton(
                           child: const Text('Save'),
                           onPressed: () {
+                            Navigator.of(context).pop();
                             _addHome();
                             _homeNameController.clear();
-                            // Navigator.of(context).pop();
                           },
                         ),
                       ],
@@ -247,10 +293,9 @@ class _HomesPageState extends State<HomesPage> {
                 );
               }
             : () {},
-        child: Icon(
+        child: const Icon(
           Icons.add_home_outlined,
           size: 50,
-          // color: homeventory.primary,
         ),
       ),
     );
